68369860320bd68f85bc451dc8c8a59e1b55fd9e
GreedNeSS <ruslan199126@gmail.com>
2022-04-08 22:15:51 +0300

    Добавьте файлы проекта.

---
+++ TPH/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TPH.Models;
+
+namespace TPH
+{
+    internal class ApplicationContext: DbContext
+    {
+        public DbSet<User> users { get; set; } = null!;
+        public DbSet<Manager> managers { get; set; } = null!;
+        public DbSet<Employee> employees{ get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=People.db");
+        }
+    }
+}
---
+++ TPH/Program.cs
@@ -0,0 +1,9 @@
+﻿using TPH;
+using TPH.Models;
+
+Console.WriteLine("***** Table Per Hierarchy *****");
+
+using (ApplicationContext db = new ApplicationContext())
+{
+
+}
